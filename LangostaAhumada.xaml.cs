e95debb9cd25492deed2e6b676775672f4925893
Lorena-lisseth <2431186@clases.edu.sv>
2023-07-19 09:07:43 -0600

    Se creo una app capaz de calcular el precio de un plato de comida

--- LangostaAhumada.xaml.cs
+++ LangostaAhumada.xaml.cs
@@ -9,7 +9,7 @@ public partial class LangostaAhumada : ContentPage
     /// <Company>USB Technology</Company>
     /// <Lastmodificationdate>08/07/2023</Lastmodificationdate>
     /// <Lastmodificationdescription>Se agrego un If el cual verificara si los datos introducidos son nulos o si los entry estan vacios</Lastmodificationdescription>
-    /// <Lastmodificationautor>Lorena Vazquez</Lastmodificationautor>
+    /// <Lastmodificationautor>Lorena Lisseth Vazquez Godoy</Lastmodificationautor>
     /// </summary>
 
     /// <summary>Constructor de la clase</summary>
